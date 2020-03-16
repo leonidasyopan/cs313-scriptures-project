8aef743c23333cfa604d7b2c3d6d52fea252133a
Leonidas Yopan <leonidasyopan@gmail.com>
2020-03-16 18:29:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, function() {
+    console.log("Server listening on port " + PORT);
+})
